574789ce8788973c76decd84883a2923baea0a40
Graeme <graemefolk@gmail.com>
2022-08-29 10:33:57 -0600

    refactor code

--- src/hooks/useInputState.js
+++ src/hooks/useInputState.js
@@ -3,8 +3,12 @@ import { useState } from 'react';
 function useInputState(initialValue) {
     const [value, setValue] = useState(initialValue);
 
-    const handleChange = e => setValue(e.target.value);
-    const resetField = () => setValue("");
+    const handleChange = e => {
+        setValue(e.target.value);
+    };
+    const resetField = () => {
+        setValue("");
+    };
 
     return [value, handleChange, resetField];
 }
