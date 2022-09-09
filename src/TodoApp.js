8a127d1970cb513661e051923d02ceb804a3012d
Graeme <graemefolk@gmail.com>
2022-09-08 19:30:23 -0600

    code cleanup

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,38 +1,41 @@
-import React from 'react'
-import { TodosProvider } from './contexts/todos.context';
-import { DarkModeProvider } from './contexts/theme.context';
-import NavBar from './NavBar';
-import { Paper, Unstable_Grid2 } from '@mui/material/';
-import TodoList from './TodoList';
-import TodoForm from './TodoForm';
+import React from "react";
+import { TodosProvider } from "./contexts/todos.context";
+import { DarkModeProvider } from "./contexts/theme.context";
+import NavBar from "./NavBar";
+import { Paper, Unstable_Grid2 } from "@mui/material/";
+import TodoList from "./TodoList";
+import TodoForm from "./TodoForm";
 
 function TodoApp() {
+  //TODO: Make this a PWA. Could be good for the budgeting app as well. Any value for the portoflio balancer? Likely not.
+  //https://create-react-app.dev/docs/making-a-progressive-web-app/
 
-    //TODO: Move completed todos to the bottom, different collapsable TodoList?
-    // const incompleteTodos = todoItems.filter(t => t.completed);
-    // const completedTodos = todoItems.filter(t => t.completed);
-
-    //TODO: Make this a PWA. Could be good for the budgeting app as well. Any value for the portoflio balancer? Likely not.
-    //https://create-react-app.dev/docs/making-a-progressive-web-app/
-
-    return (
-        <DarkModeProvider>
-            <Paper style={{
-                padding: 0, margin: 0, height: "100vh"
-            }}
-                elevation={0}>
-                <NavBar />
-                <Unstable_Grid2 container justifyContent='center' style={{ marginTop: "1rem" }}>
-                    <Unstable_Grid2 item xs={11} md={8} lg={4}>
-                        <TodosProvider>
-                            <TodoForm />
-                            <TodoList />
-                        </TodosProvider>
-                    </Unstable_Grid2>
-                </Unstable_Grid2>
-            </Paper>
-        </DarkModeProvider>
-    )
+  return (
+    <DarkModeProvider>
+      <Paper
+        style={{
+          padding: 0,
+          margin: 0,
+          height: "100vh",
+        }}
+        elevation={0}
+      >
+        <NavBar />
+        <Unstable_Grid2
+          container
+          justifyContent="center"
+          style={{ marginTop: "1rem" }}
+        >
+          <Unstable_Grid2 item xs={11} md={8} lg={4}>
+            <TodosProvider>
+              <TodoForm />
+              <TodoList />
+            </TodosProvider>
+          </Unstable_Grid2>
+        </Unstable_Grid2>
+      </Paper>
+    </DarkModeProvider>
+  );
 }
 
-export default TodoApp
+export default TodoApp;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,29 +1,27 @@
-import React, { useContext } from 'react';
-import { TodosContext } from './contexts/todos.context';
-import { Paper, List, Divider } from '@mui/material/';
-import TodoItem from './TodoItem';
+import React, { useContext } from "react";
+import { TodosContext } from "./contexts/todos.context";
+import { Paper, List, Divider } from "@mui/material/";
+import TodoItem from "./TodoItem";
 
 function TodoList(props) {
-    const todoItems = useContext(TodosContext);
+  const todoItems = useContext(TodosContext);
 
-    if (todoItems.length > 0) {
-        return (
-            <Paper>
-                <List>
-                    {todoItems.map((t, idx) => (
-                        <React.Fragment key={t.id}>
-                            <TodoItem
-                                todo={t}
-                            />
-                            {(idx < todoItems.length - 1) && <Divider />}
-                        </React.Fragment>
-                    ))}
-                </List>
-            </Paper>
-        )
-    } else {
-        return null;
-    }
+  if (todoItems.length > 0) {
+    return (
+      <Paper>
+        <List>
+          {todoItems.map((t, idx) => (
+            <React.Fragment key={t.id}>
+              <TodoItem todo={t} />
+              {idx < todoItems.length - 1 && <Divider />}
+            </React.Fragment>
+          ))}
+        </List>
+      </Paper>
+    );
+  } else {
+    return null;
+  }
 }
 
-export default TodoList
+export default TodoList;
